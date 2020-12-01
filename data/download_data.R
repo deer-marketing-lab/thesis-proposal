a9eecfaae30337a7ed4793778ee9654c78b59ce7
Lachlan Deer <lachlan.deer@gmail.com>
2020-11-30 18:44:36 -0600

    force add script to download data

---
+++ data/download_data.R
@@ -0,0 +1,16 @@
+#' download_data.R
+#'
+#' contributors: INSERT_NAME
+#'
+#' Explain what this file does
+#'
+
+# Libraries
+## NOTES: Add the libraries you need for this script to run sucessfully
+
+# YOUR CODE HERE
+## NOTES: Do add comments
+
+# Save Data to File
+## NOTES: Use the end of the script to save the data
+## Example: write_csv(my_df, here("data", "my_dataset.csv"))
